f42dca4b9a728abb497323d5f6c0e01f0451b145
aamorifreeman <164108283+aamorifreeman@users.noreply.github.com>
2025-05-15 01:00:42 -0400

    Initial Commit

---
+++ UrgeButton/UrgeButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrgeButtonApp.swift
+//  UrgeButton
+//
+//  Created by Aamori Freeman on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct UrgeButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UrgeButtonTests/UrgeButtonTests.swift
@@ -0,0 +1,17 @@
+//
+//  UrgeButtonTests.swift
+//  UrgeButtonTests
+//
+//  Created by Aamori Freeman on 5/15/25.
+//
+
+import Testing
+@testable import UrgeButton
+
+struct UrgeButtonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
